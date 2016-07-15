0bff2e2d1e11f8b5d902a76eab830baa0776be98
Tprezioso <tommyprezioso@gmail.com>
2016-07-15 16:07:09 -0400

    added twitter call but has error

--- BLUKeyboard/BLUKeyboardViewController.swift
+++ BLUKeyboard/BLUKeyboardViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import Social
+import Accounts
 
 extension String {
     
@@ -35,6 +37,7 @@ class BLUKeyboardViewController: UIInputViewController, UIPopoverControllerDeleg
     var didTapSpaceForSpellCheck: Bool!
     var popoverView: UIView!
     var tableView:UITableView!
+    var dataSource = [AnyObject]()
     var items: [String] = ["Viper", "X", "Games"]
     
     override func viewDidAppear(animated: Bool) {
@@ -68,6 +71,45 @@ class BLUKeyboardViewController: UIInputViewController, UIPopoverControllerDeleg
     
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         print("You selected cell #\(indexPath.row)!")
+    }
+    func getTimeLine() {
+        
+        let account = ACAccountStore()
+        let accountType = account.accountTypeWithAccountTypeIdentifier(
+            ACAccountTypeIdentifierTwitter)
+        
+        account.requestAccessToAccountsWithType(accountType, options: nil, completion: {(success: Bool, error: NSError!) -> Void in
+            if success {
+                let arrayOfAccounts =
+                account.accountsWithAccountType(accountType)
+                if arrayOfAccounts.count > 0 {
+                    let twitterAccount = arrayOfAccounts.last as! ACAccount
+                    let requestURL = NSURL(string:"https://api.twitter.com/1.1/statuses/user_timeline.json")
+                    let parameters = ["screen_name" : "@techotopia","include_rts" : "0","trim_user" : "1", "count" : "20"]
+                                                            
+                    let postRequest = SLRequest(forServiceType:SLServiceTypeTwitter,
+                                                 requestMethod: SLRequestMethod.GET,
+                                                           URL: requestURL,
+                                                    parameters: parameters)
+                                                            
+                    postRequest.account = twitterAccount
+                                                            
+                    postRequest.performRequestWithHandler({(responseData: NSData!,
+                                                            urlResponse: NSHTTPURLResponse!,
+                                                            error: NSError!) -> Void in
+                                                            var err: NSError?
+                                                            self.dataSource = NSJSONSerialization.JSONObjectWithData(responseData, options: NSJSONReadingOptions.MutableLeaves, error: err) as! [AnyObject]
+                        if self.dataSource.count != 0 {
+                            dispatch_async(dispatch_get_main_queue()) {
+                            self.tableView.reloadData()
+                            }
+                        }
+                                                        })
+                    }
+            } else {
+                print("Failed to access account")
+            }
+        })
     }
 
     
